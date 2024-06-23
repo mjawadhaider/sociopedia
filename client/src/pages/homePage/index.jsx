3caba495c1b00f16b024d09e053e802496bfe9af
mjawadhaider <m.jawadhaider03@gmail.com>
2024-06-23 18:59:39 +0500

    Create Navbar

---
+++ client/src/pages/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import NavBar from 'pages/navbar';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
+};
+
+export default HomePage;
