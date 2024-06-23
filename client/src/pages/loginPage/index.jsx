cb430f4a84536a4425008230654637c55eeb9b99
mjawadhaider <m.jawadhaider03@gmail.com>
2024-06-23 18:54:48 +0500

    Create Login Page

---
+++ client/src/pages/loginPage/index.jsx
@@ -0,0 +1,32 @@
+import { useTheme } from '@emotion/react';
+import { Box, Typography, useMediaQuery } from '@mui/material';
+import Form from './Form';
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+  return (
+    <Box>
+      <Box width="100%" backgroundColor={theme.palette.background.alt}>
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? '50%' : '93%'}
+        p="2rem"
+        m="2rem auto"
+        borderRadius='1.5rem'
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant='h5' sx={{ mb: '1.5rem'}}>
+          Welcome to Sociopedia, the social media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
